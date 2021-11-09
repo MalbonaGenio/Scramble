bef4a7883f5eb08ad7bb3ed82188da9ecca51344
Pau Valverde <pvalverdemolina@gmail.com>
2021-11-09 18:43:10 +0100

    Initial Commit

---
+++ Scramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scramble
+//
+//  Created by Pau Valverde Molina on 9/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scramble/ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrambleApp.swift
+//  Scramble
+//
+//  Created by Pau Valverde Molina on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
